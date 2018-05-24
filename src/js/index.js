8c778a612d813867b5e4766f557e535d7d85c36a
mik3dev <miguel.acosta1978@gmail.com>
2018-05-23 21:42:51 -0400

    Add some html, scss and js files

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,5 @@
 import "../scss/style.scss";
 
-function component(){
-	const element = document.createElement('div');
-	element.innerHTML = "<h1>Hello World</h1>"
-	return element;
-}
-
-document.querySelector('body').appendChild(component());
-console.log("Hello World");
+document.querySelector('.button').addEventListener('click', function(){
+	alert('Hello World');
+});
